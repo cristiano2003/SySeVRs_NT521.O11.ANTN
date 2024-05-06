e072abb148369adf730349b14cd82a17b751b7fc
Trung Trực Nguyễn <trungtrucnguyen@192.168.0.100>
2024-05-06 15:26:47 +0700

    fix

--- config.sh
+++ config.sh
@@ -2,11 +2,11 @@
 
 # Skip this if you already user ubuntu18.04 
 #-----------install python3 and set to use-------------------------#
-sudo add-apt-repository ppa:ubuntu-toolchain-r/ppa
-sudo apt install python3.7
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 2
-sudo update-alternatives --config python3
+# sudo add-apt-repository ppa:ubuntu-toolchain-r/ppa
+# sudo apt install python3.7
+# sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
+# sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 2
+# sudo update-alternatives --config python3
 #--------------------------------------------------#
 # =========================== #
 # ==== setup environment ==== #
